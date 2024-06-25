d240795b24eaa43d3ec25d4f8ffc83b739d3ca5e
dev-shreya <shreyaabond005@gmail.com>
2024-06-24 20:35:34 -0700

    Parameterized pipes

--- src/shorten.pipe.ts
+++ src/shorten.pipe.ts
@@ -5,9 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone:true
 })
 export class ShortenPipe implements PipeTransform {
-  transform(value: string) {
-    if (value.length > 100) {
-      return value.substr(0, 80) + ' ...';
+  transform(value: string,limit:number) {
+    if (value.length > limit) {
+      return value.substr(0, limit) + ' ...';
     } else {
       return value;
     }
